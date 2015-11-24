06eb51288fecee3afede09c0dc2b8a016d7c74d2
kvpradap <pradap@cs.wisc.edu>
2015-11-24 17:25:52 -0600

    cleaned up a bit
    cleaned up a bit

--- dist/Magellan-0.1/build/lib/magellan/blocker/blocker.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import logging
-logger = logging.getLogger(__name__)
-class Blocker(object):
-    pass
-    # remove nows with nan values at block_attr
-    def rem_nan(self, table, block_attr):
-        l = table.index.values[np.where(table[block_attr].notnull())[0]]
-        return table.ix[l]
